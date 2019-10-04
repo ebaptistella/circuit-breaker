788cc79c8e758f6f66e9321517fc8f453a194255
Eurides V. Baptistella <eurides@sysmo.com.br>
2019-10-04 17:24:41 -0300

    Doc: Faz o mapeamento dos objetos que serão trafegados

---
+++ src/main/java/com/github/ebaptistella/circuitbreaker/dto/MicrorregiaoDTO.java
@@ -0,0 +1,26 @@
+package com.github.ebaptistella.circuitbreaker.dto;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+@XmlRootElement
+@ApiModel
+public class MicrorregiaoDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(notes = "Código da Microrregião")
+    private Long id;
+
+    @ApiModelProperty(notes = "Nome da Microrregião")
+    private String nome;
+
+    @ApiModelProperty(notes = "Mesorregião que a microrregião pertence")
+    private MesorregiaoDTO mesorregiaoDTO;
+}
