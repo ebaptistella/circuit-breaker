6a03cfc333bb8edcc0a395d2fdc2282da6a9a5c1
Euride Baptistella <euride.baptistella@gmail.com>
2019-10-09 21:33:27 -0300

    Doc: Inicio testes unitário para service

--- src/test/java/com/github/ebaptistella/circuitbreaker/api/CityControllerTest.java
+++ src/test/java/com/github/ebaptistella/circuitbreaker/api/CityControllerTest.java
@@ -31,13 +31,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.github.ebaptistella.circuitbreaker.dto.MunicipioRetornoDTO;
 import com.github.ebaptistella.circuitbreaker.service.CityService;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @WebMvcTest(CityController.class)
 @ActiveProfiles("test")
 public class CityControllerTest {
